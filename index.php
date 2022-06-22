90cb7ef33003d0155ecc5bacee2dfd2807ab8b88
Enzo Russi <enzrussi@gmail.com>
2022-06-22 16:55:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 <html>
   <head>
     <title>Questura Trento - UTL - 4^ Sezione Informatica e Teecomunicazioni</title>
-    
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <script src="./js/bootstrap.bundle.min.js"></script>
   </head>
   <body>
-  
+    <div class="container">
+    <h1> Dashboard TLC <h1>
+    
+    </div>
   </body>
 </html>
