1251547a4f446bee069d59b16f18e9e61dd9a2f5
Enzo Russi <enzrussi@gmail.com>
2022-06-23 08:19:11 +0200

    update index.php and links.json

--- index.php
+++ index.php
@@ -15,19 +15,29 @@
             </a>
           </div>
         </nav>
+      
       <?php
 
+        //leggo il file json
         $jsonfile = file_get_contents("links.json");
         $json = json_decode($jsonfile);
-        foreach ($json->links as $j){
 
-          print_r($j);
-          echo "<br>";
+        //estraggo i dati e creo le varie card
+        foreach ($json->links as $j){
   
           echo "Title:" .$j->title;
           echo "Description: ". $j->description;
           echo "Url: ".$j->url;
           echo "<br>";
+
+          echo '<div class="card" style="width: 18rem;">
+                <div class="card-body">
+                <h5 class="card-title">'.$j->title.'</h5>
+                <h6 class="card-subtitle mb-2 text-muted">'.$j->subtitle.'</h6>
+                <p class="card-text">'.$j->description.'</p>
+                <a href="'.$j->url.'" class="card-link">Clicca qui</a>
+                </div>
+                </div>';
         }
         
 
